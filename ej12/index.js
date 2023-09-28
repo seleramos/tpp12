80784edf0ea5991d7b01bfdc4337cd1bbdec36b9
seleramos <106417738+seleramos@users.noreply.github.com>
2023-09-28 08:57:31 -0300

    Add files via upload

---
+++ ej12/index.js
@@ -0,0 +1,5 @@
+function cambiarEstiloLista() {
+    let pun = document.getElementById('paises')
+    let punselect = document.getElementById('estilo')
+    pun.style.listStyleType = punselect.value
+}
